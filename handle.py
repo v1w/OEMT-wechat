b50cd5143f05b1b0e2c39e3bc282b6654426deb6
v1w <v1w@users.noreply.github.com>
2016-10-10 23:49:20 +0800

    Add reservation

--- handle.py
+++ handle.py
@@ -3,6 +3,7 @@ import requests
 import json
 import traceback
 from bs4 import BeautifulSoup
+import re
 
 HELP_MSG = \
 """命令格式:
@@ -19,7 +20,22 @@ EQUIP_ID = {
     'coater': '16a07cd3-f77b-42b5-81f8-21b7209d3a8d'
 }
 
-MAINTENENCE_MSG = "维护中.."
+MAINTAINENCE_MSG = "维护中.."
+
+
+class Session(object):        
+    def __init__(self, username, passwd):
+        self.post_data = {
+            'HomeLoginType': '1',
+            'LoginName': username,
+            'LoginPassword': passwd,
+            'IsRememberMe': 'true',
+            'loginType': '1'
+        }
+        resp_raw = requests.post(OEMT_HOST + '/Account/LoginSubmit', data=self.post_data, timeout=3)
+
+        self.cookies = resp_raw.cookies
+
 
 def handle_content(msg):
     try:
@@ -29,6 +45,8 @@ def handle_content(msg):
         if len(args) == 1:
             if args[0].lower() == 'help':
                 return HELP_MSG
+            if args[0].lower() == 'debug':
+                return reserve_equip(fromuser, 'sem', 1, 2)
         elif len(args) == 2:
             if args[1].lower() == 'now':
                 return get_status(args[0].lower())
@@ -37,12 +55,15 @@ def handle_content(msg):
         elif len(args) == 3:
             if args[0].lower() == 'bind':
                 return bind_user(fromuser, args[1], args[2])
+        elif len(args) == 10:
+            if args[1] == 'res':
+                return reserve_equip(fromuser, args[0].lower(), int(args[3]), args[5], float(args[7]), args[9])
 
         return SYNTAX_ERR_MSG
 
     except Exception as e:
         print(e)
-        #traceback.print_exc()
+        traceback.print_exc()
         return "Internal Error"
 
 
@@ -52,7 +73,7 @@ def get_status(equipment):
     try:
         resp_raw = requests.get(OEMT_HOST + '/Equipment/GetEquipmentCurStatusInfo?Id=' + EQUIP_ID[equipment], timeout=3)
     except requests.exceptions.Timeout:
-        return MAINTENENCE_MSG
+        return MAINTAINENCE_MSG
     resp = json.loads(resp_raw.text)
     return resp.get('Remark', 'Server Error, please try again.')
 
@@ -69,7 +90,7 @@ def get_reserve_status(equipment, day):
     try:
         resp_raw = requests.post(OEMT_HOST + '/Equipment/AppointmentTimesContainer', data=post_data, timeout=3)
     except requests.exceptions.Timeout:
-        return MAINTENENCE_MSG
+        return MAINTAINENCE_MSG
     resrv_data = BeautifulSoup(resp_raw.text, "lxml").find(id="tbAppointmentTimes")
     #cur_date = str(datetime.date.today())
     #d1_date = str(datetime.date.today() + datetime.timedelta(days=1))
@@ -114,7 +135,7 @@ def bind_user(openid, username, passwd):
     try:
         resp_raw = requests.post(OEMT_HOST + '/Account/LoginSubmit', data=post_data, timeout=3)
     except requests.exceptions.Timeout:
-        return MAINTENENCE_MSG
+        return MAINTAINENCE_MSG
     if resp_raw.text == '出错,用户名和密码不正确!':
         return "用户名密码错误(＞﹏＜)"
 
@@ -128,7 +149,7 @@ def bind_user(openid, username, passwd):
 
     return "绑定成功！"
 
-def reserve_equip(openid, equipment, t_start, t_end):
+def reserve_equip(openid, equipment, date, t_start, duration, comment):
     try:
         with open('./users.json') as f:
             users = json.loads(f.read())
@@ -136,21 +157,58 @@ def reserve_equip(openid, equipment, t_start, t_end):
         return "请先绑定用户名密码！" 
     if openid not in users.keys():
         return "请先绑定用户名密码！"
+    if equipment not in EQUIP_ID.keys():
+        return "无法识别的仪器名称！"
 
     username = users[openid][0]
     passwd = users[openid][1]
 
-    post_data = {
-        'HomeLoginType': '1',
-        'LoginName': username,
-        'LoginPassword': passwd,
-        'IsRememberMe': 'true',
-        'loginType': '1'
-    }
+    appoint_periods_num = int(duration/0.5)
+    if appoint_periods_num <= 0:
+        return "最小预约单位为半小时！"
+    appoint_start = list(map(int, t_start.split(':')))
+    if appoint_start[1] not in [0,30]:
+        return "不允许预约非整数时间段！"
+    start_min = appoint_start[0] * 60 + appoint_start[1]
+    periods = [datetime.datetime.strptime(str(datetime.date.today()), '%Y-%m-%d') + datetime.timedelta(days=date, minutes=start_min + 30 * i) \
+                for i in range(0, appoint_periods_num)]
+    appoint_times = ','.join([period.strftime('%Y-%m-%d %H:%M') for period in periods])
+    print(appoint_times)
+
     try:
-        resp_raw = requests.post(OEMT_HOST + '/Account/LoginSubmit', data=post_data, timeout=3)
+        session = Session(username, passwd)
+        post_data = {
+        'equipmentTimeAppointmemtMode': '0',
+        'equipmentId': EQUIP_ID[equipment],
+        'userId': None,
+        'changeAppointmentId': None,
+        'date': None
+        }
+        resp_raw = requests.post(OEMT_HOST + '/Appointment/AppointmentUserRelativeInfo', data=post_data, timeout=3, cookies=session.cookies)
+        m = re.search(r'"Id":".*?"', resp_raw.text)
+        subject_id = m.group(0)[6:][:-1]
+        post_data = {
+        'SubjectId': subject_id,
+        'UseNature': '0',
+        'ExperimentationContent': comment,
+        'isSelectTimeScope': 'false',
+        'AppointmentStep': '30',
+        'AppointmentTimes': appoint_times,
+        'EquipmentId': EQUIP_ID[equipment],
+        'AppointmentFeeTips': 'false'
+        }
+        resp_raw = requests.post(OEMT_HOST + '/Appointment/Appointment', data=post_data, timeout=3, cookies=session.cookies)
+        m = re.search(r'出错', resp_raw.text)
+        if m is not None:
+            return resp_raw.text
+        else:
+            return "预约成功！"
     except requests.exceptions.Timeout:
-        return MAINTENENCE_MSG
+        return MAINTAINENCE_MSG
+
+    
+
+
 
     
 
